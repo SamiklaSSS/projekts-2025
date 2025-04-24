ca7d50220dfdb4e260af0a40df32ecf973a6eb50
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2025-04-24 09:23:23 +0300

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,12 @@
+questions = [
+{
+"question": "Ūdens varīšanas temperatūta pie zemes virsmas ir...",
+"options": {"A": "90°C", "B": "100°C", "C": "110°C", "D": "120°C"},
+"correct": "B"
+},
+{
+"question": "Kāda bija visizplātītāka valūta pasaulē 1900. gadā?",
+"options": {"A": "Sterliņu mārciņš", "B": "Rubls", "C": "Eiro", "D": "Vācu marka"},
+"correct": "A"
+}
+]
