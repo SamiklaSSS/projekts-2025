fbc6b7fd08a12aa69743f5d3ff9d059c0c047087
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2025-05-08 08:42:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ from questions import questions
 console = Console()
 print("[bold blue]Tests![/bold blue]")
 
+#punktu skaitīšanaatkarībā no laika
 def calculate_score(start, end):
     elapsed = end - start
-    score = max(0, int(1000 - elapsed * 50))
+    score = max(0, int(1000 - elapsed * 50)) 
     return score
 
+#laiks lietotājam izlasīt jautājumu un domāt
 def simulate_timer(seconds):
     for _ in track(range(seconds), description="[yellow]Laiks domāt...[/yellow]"):
         time.sleep(1)
 
+#jautājumu skaits, viktorīnas ilgums
 def start_quiz():
     total_score = 0
     question_count = len(questions)
 
+#rāda lietotājam jautājumu kopējo skaitu, un jautājumu uz kuru lietotājs atbild šobrīd
     for index, q in enumerate(questions, start=1):
         console.rule(f"[bold yellow]Jautājums {index} no {question_count}[/bold yellow]")
         print(f"[bold]{q['question']}[/bold]")
 
+#atbildes
         for key, value in q["options"].items():
             print(f"[cyan]{key}:[/cyan] {value}")
 
@@ -33,11 +38,13 @@ def start_quiz():
         start_time = time.time()
         answer = input("\nTava atbilde (A/B/C/D): ").strip().upper()
         end_time = time.time()
+        
 #validācija
         if answer in "ABCD":
             print("[green]Atbilde ir pieņemta![/green]")
         else:
             print("[red]Atbilde nav pieņemta![/red]")
+            
 #pārbaude vai atbilde ir pareiza
         if answer == q["correct"]:
             score = calculate_score(start_time, end_time)
